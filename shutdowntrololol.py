17c0a0f1b74fb436a7c6916f185a229163186c32
Orbernator <159992555+Orbernator@users.noreply.github.com>
2024-04-30 12:37:40 +0930

    Update shutdowntrololol.py
    made it better

--- shutdowntrololol.py
+++ shutdowntrololol.py
@@ -1,15 +1,17 @@
 import os
 import time
 from tkinter import *
-root = Tk()
-root.title("SAY GOODBYE")
-root.minsize(200, 200)  # width, height
-root.geometry("300x300+50+50")
-text = Label(root, text="TROLLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOl")
-text.pack()
-text2 = Label(root, text="COMPUTER SAY BYE BYE")
-text2.pack()
-root.mainloop()
+for x in range(11):
+    root = Tk()
+    root.title("SAY GOODBYE")
+    root.minsize(200, 200)  # width, height
+    root.geometry("300x300+50+50")
+    text = Label(root, text="TROLLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOl")
+    text.pack()
+    text2 = Label(root, text="COMPUTER SAY BYE BYE")
+    text2.pack()
+    root.mainloop()
+
 print ("BYE BYE")
 os.system('shutdown /s')
 time.sleep(1)

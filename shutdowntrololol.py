8b2dd741d55b75f25627a4db720648f1bd5f9a03
Orbernator <159992555+Orbernator@users.noreply.github.com>
2024-04-30 12:29:30 +0930

    Create shutdowntrololol.py
    Just shuts down it

---
+++ shutdowntrololol.py
@@ -0,0 +1,14 @@
+import os
+import time
+from tkinter import *
+root = Tk()
+root.title("SAY GOODBYE")
+root.minsize(200, 200)  # width, height
+root.geometry("300x300+50+50")
+text = Label(root, text="TROLLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOl")
+text.pack()
+text2 = Label(root, text="COMPUTER SAY BYE BYE")
+text2.pack()
+root.mainloop()
+print ("BYE BYE")
+os.system('shutdown /s')

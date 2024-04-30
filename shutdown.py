60f9d5ed6a6a68b7032c6aea3bbdfc3d6ee81f54
Orbernator <159992555+Orbernator@users.noreply.github.com>
2024-04-30 12:03:06 +0930

    Shutdown.py
    Shuts down pc
    Warning version

---
+++ shutdown.py
@@ -0,0 +1,31 @@
+import os
+import time
+print ("WARNING")
+time.sleep(0.5)
+print("THIS SOFTWARE CAN DO DAMAGE TO UNSAVED SOFTWARE")
+time.sleep(0.5)
+print("THE CREATOR IS IN NO WAY LIABLE FOR ANY DAMAGES CAUSED BY THIS SOFTWARE")
+time.sleep(0.5)
+print("BY CONTINUING YOU ARE UNDERSTANDING THE RISKS INVOLVED")
+time.sleep(0.5)
+start = input ("Do you wish to continue? (Y/N)")
+def mainStart():
+    if start == ("Y"):
+        print("Starting")
+        os.system('cls')
+        os.system('shutdown /s')
+    elif start == ("N"):
+        print("Canceled")
+        time.sleep(1)
+        os.system('exit')
+    elif start == ("y"):
+        print("Starting")
+        os.system('cls')
+        os.system('shutdown /s')
+    elif start == ("n"):
+        print("Canceled")
+        time.sleep(1)
+        os.system('exit')
+    else:
+        print ("Incorrect Command")
+        mainStart()
